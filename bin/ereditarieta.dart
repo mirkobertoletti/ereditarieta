b50daff94ef3366128d64b57a22616e452659ffa
mirkobertoletti <mirkobertoletti@gmail.com>
2025-10-18 23:42:14 +0200

    first commit

---
+++ bin/ereditarieta.dart
@@ -0,0 +1,5 @@
+import 'package:ereditarieta/ereditarieta.dart' as ereditarieta;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ereditarieta.calculate()}!');
+}
---
+++ test/ereditarieta_test.dart
@@ -0,0 +1,8 @@
+import 'package:ereditarieta/ereditarieta.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
